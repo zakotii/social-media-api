c0e3f76efcac72c105a522f0e9b661439a85359d
zakotii <abc1234567892777@gmail.com>
2025-03-06 21:21:50 +0200

    feat: add user API (list & detail views)

--- socialmedia/api/views.py
+++ socialmedia/api/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.contrib.auth.models import User
+from rest_framework import generics
+from .serializers import UserSerializer
+
+class UserListView(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+class UserDetailView(generics.RetrieveAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
