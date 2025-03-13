dc17ff51b0d6619aee172862b52402487d928201
zakotii <abc1234567892777@gmail.com>
2025-03-13 15:18:40 +0200

    Add token verification endpoint

--- socialmedia/socialmedia/urls.py
+++ socialmedia/socialmedia/urls.py
@@ -17,7 +17,11 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+    TokenVerifyView,
+)
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 urlpatterns = [
@@ -31,4 +35,5 @@ urlpatterns = [
         SpectacularSwaggerView.as_view(url_name="schema"),
         name="swagger-ui",
     ),
+    path("api/token/verify/", TokenVerifyView.as_view(), name="token_verify"),
 ]
