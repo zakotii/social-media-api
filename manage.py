0d7a12b4d71172758385fc5f811016e751e67139
zakotii <abc1234567892777@gmail.com>
2025-03-16 21:27:39 +0200

    moved the README.md file to the root of the project, moved settings.py to the socialmedia folder

--- manage.py
+++ manage.py
@@ -8,7 +8,7 @@ sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "socialmedia.settings")
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
